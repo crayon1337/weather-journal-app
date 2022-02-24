5c563518114dcbc990140443329eb8a0cc845c92
Crayon <darkspear123@outlook.com>
2022-02-24 05:06:14 +0200

    added apiKey const, click Event Listener to the button

--- website/app.js
+++ website/app.js
@@ -1,8 +1,15 @@
 // Personal API Key for OpenWeatherMap API
+const apiKey = '124e10c3c63a51cd59f7a6c15edab0a6';
 
 // Event listener to add function to existing HTML DOM element
+document.getElementById('generate').addEventListener('click', handleClick);
 
 /* Function called by event listener */
+function handleClick(e) {
+    e.preventDefault();
+
+    console.log('test');
+}
 
 /* Function to GET Web API Data*/
 
